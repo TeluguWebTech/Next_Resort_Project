015b4056e2235c3b3c641b9cc59341ad59417f30
Kalyan Devara <kalyan.vza@gmail.com>
2024-08-31 19:37:21 +0530

    register error added

--- src/app/components/RegisterForm.jsx
+++ src/app/components/RegisterForm.jsx
@@ -10,6 +10,8 @@ const RegisterForm = () => {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
 
     const router = useRouter()
 
