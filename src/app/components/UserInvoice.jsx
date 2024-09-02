344fc00ce2071a3872793cd1f1220385853583a7
Kalyan Devara <kalyan.vza@gmail.com>
2024-09-02 19:27:59 +0530

    invoice loader

--- src/app/components/UserInvoice.jsx
+++ src/app/components/UserInvoice.jsx
@@ -1,19 +1,16 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { Circles } from 'react-loader-spinner'
 
 const UserInvoice = ({ userId }) => {
   const [invoice, setInvoice] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-
   console.log("extractId:", userId);
 
 
   const invoiceHandler = async () => {
-    setError(true)
     try {
       const response = await fetch(`https://next-resort-project.vercel.app/api/users/${userId}`);
       const newData = await response.json();
@@ -37,15 +34,7 @@ const UserInvoice = ({ userId }) => {
   }, []);
 
   if (loading) {
-    return <div><Circles
-    height="80"
-    width="80"
-    color="#4fa94d"
-    ariaLabel="circles-loading"
-    wrapperStyle={{}}
-    wrapperClass=""
-    visible={true}
-    /></div>;
+    return <div>Loading...</div>;
   }
 
   if (error) {
