3016fe550ab64e721ff405b35e79896287936a76
Kalyan Devara <kalyan.vza@gmail.com>
2024-09-02 17:15:29 +0530

    optimized

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
+import "../../public/styles/styles.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
